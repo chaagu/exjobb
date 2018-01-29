7c750123e289a1aed17877054ab6aef091339345
Medina Velic <medvel@qrtech.se>
2018-01-29 16:17:59 +0100

    new function

---
+++ test/plot_func.py
@@ -0,0 +1,11 @@
+def plot_func(vector):
+    import matplotlib.pyplot as plt
+    plt.plot(vector)
+    plt.ylabel('something')
+    plt.xlabel('something else')
+    plt.title('helloo')
+    plt.show()
+
+from import random_array
+
+plot_func(random_array)
