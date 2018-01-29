aa50c54935f4db21d6686940e583f5a966e01064
Medina Velic <medvel@qrtech.se>
2018-01-29 16:32:49 +0100

    final final version

--- test/plot_func.py
+++ test/plot_func.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 import matrixGeneration
 
 def plot_func(vector):
+    """
     plt.plot(vector)
     plt.ylabel('something')
     plt.xlabel('something else')
     plt.title('helloo')
     plt.show()
+    """
+    print(vector)
 
 array = matrixGeneration.random_array()
 
